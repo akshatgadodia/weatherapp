1fa2d113a8f14a69d5e733bd4405a9ef1d5ad503
Akshat Gadodia <akshatgadodia@gmail.com>
2023-06-20 20:01:25 +0530

    Weather App Updated

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import'./TopBarCSS.css'
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import React from "react";
+import "./TopBarCSS.css";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
 const TopBar = () => {
   return (
-    
-    <div className='top-bar'>
-        <span className='top-bar-head'><WbSunnyIcon className='top-bar-head-icon' fontSize='large'/>
-        WEATHER<WbSunnyIcon className='top-bar-head-icon' fontSize='large'/></span>
-        <span className="top-bar-info"><a href="https://akshatgadodia.github.io/" target='_blank' rel="noreferrer">
-          About Me</a></span>
+    <div className="top-bar">
+      <span className="top-bar-head">
+        <WbSunnyIcon className="top-bar-head-icon" fontSize="large" />
+        <h1>WEATHER</h1>
+        <WbSunnyIcon className="top-bar-head-icon" fontSize="large" />
+      </span>
+      <span className="top-bar-info">
+        <a
+          href="https://akshatgadodia.github.io/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          About Me
+        </a>
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
